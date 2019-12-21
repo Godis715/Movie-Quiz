4b07a757dc69edeac0fc65d98b5daa36d470f440
unknown <godis7.15@gmail.com>
2019-12-21 18:38:40 +0300

    Added routing

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
+import About from './components/About.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router: new VueRouter({
+    routes: [
+      {
+        path: '/',
+        name: 'HelloWorld',
+        component: HelloWorld
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: About
+      }
+    ],
+  })
+})
